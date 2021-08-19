64aa9dd228d69cdd7af79983c8ed80af1cd25676
Jun Shim <wowns1537@gmail.com>
2021-08-19 14:12:19 +0900

    Create 1-Two_Sum.py

---
+++ LeetCode/1-Two_Sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     result = None
+
+    #     for a, num_a in enumerate(nums):
+    #         for b, num_b in enumerate(nums[a+1:], a+1):
+    #             if num_a + num_b == target:
+    #                 result = [a, b]
+
+    #     return result
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        results = dict()
+
+        for i, num in enumerate(nums):
+            result = target - num
+
+            if result in results:
+                return [results.get(result), i]
+            else:
+                results[num] = i
