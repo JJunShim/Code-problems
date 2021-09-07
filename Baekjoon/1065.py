a601fca98dea18f925baeb82645175c96ada9a30
J-JunShim <wowns1537@gmail.com>
2021-09-07 23:38:46 +0900

    update 1065.py

--- Baekjoon/1065.py
+++ Baekjoon/1065.py
@@ -3,4 +3,5 @@ def a(n: int) -> bool:
     return n // 10 % 10 * 2 == n // 100 + n % 10
 
 
-print(sum(1 if i < 100 else a(i) for i in range(1, int(input())+1)))
+n = int(input())
+print(n if n < 100 else 99 + sum(a(i) for i in range(100, n + 1)))
