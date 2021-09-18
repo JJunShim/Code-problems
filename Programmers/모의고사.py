6b3779160d8c21657e71389744a224d37db2b6c9
J-JunShim <jjunsh.120@gmail.com>
2021-09-18 22:19:12 +0900

    Update 모의고사.py

--- Programmers/모의고사.py
+++ Programmers/모의고사.py
@@ -2,11 +2,11 @@ from math import ceil
 
 
 def solution(answers: list):
-    students = {1: (1, 2, 3, 4, 5), 2: (2, 1, 2, 3, 2, 4, 2, 5),
-                3: (3, 3, 1, 1, 2, 2, 4, 4, 5, 5)}
+    students = (1, 2, 3, 4, 5), (2, 1, 2, 3, 2, 4,
+                                 2, 5), (3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
     answer = []
     n = 0
-    for k, v in students.items():
+    for k, v in enumerate(students, 1):
         m = sum(a == b for a, b in zip(
             answers, v * ceil(len(answers) / len(v))))
         if n <= m:
