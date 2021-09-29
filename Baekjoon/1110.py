c8186014e078429bb62d88ffb801fc01717594eb
J-JunShim <jjunsh.120@gmail.com>
2021-09-29 21:55:32 +0900

    Update 1110.py

--- Baekjoon/1110.py
+++ Baekjoon/1110.py
@@ -1,11 +1,10 @@
 # 더하기 사이클
 n = m = int(input())
 i = 0
-
 while True:
-    m = m % 10 * 10 + sum(divmod(m, 10)) % 10
+    q, r = divmod(m, 10)
+    m = r * 10 + (q + r) % 10
     i += 1
     if m == n:
         break
-
 print(i)
