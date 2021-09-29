b5f6a140d9dbd3e961308349750a57127af870c5
J-JunShim <jjunsh.120@gmail.com>
2021-09-29 18:58:16 +0900

    Update 11022.py

--- Baekjoon/11022.py
+++ Baekjoon/11022.py
@@ -1,12 +1,6 @@
 # A+B - 8
 import sys
-
-
-def print_case(i, a, b):
-    print(f'Case #{i}: {a} + {b} = {a + b}')
-
-
 _ = input()
 for i, l in enumerate(sys.stdin, 1):
-    a, b = map(int, l.split())
-    print_case(i, a, b)
+    l = [int(_) for _ in l.split()]
+    print(f'Case #{i}: {l[0]} + {l[1]} = {sum(l)}')
