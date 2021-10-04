778cf41965a84a720175dd0d4764d16652771727
J-JunShim <jjunsh.120@gmail.com>
2021-10-04 16:32:50 +0900

    Update 2562.py

--- Baekjoon/2562.py
+++ Baekjoon/2562.py
@@ -1,6 +1,4 @@
 # 최댓값
-from math import prod
-a = [0] * 10
-for k in str(prod((int(input()) for _ in range(3)))):
-    a[int(k)] += 1
-print(*a)
+a = {int(input()): i for i in range(1, 10)}
+b = max(a)
+print(b, a.get(b))
