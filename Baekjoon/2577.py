89b582dfb6e03394cb7fe50f90e616c63a1bb7df
J-JunShim <jjunsh.120@gmail.com>
2021-10-04 23:18:28 +0900

    Update 2577.py

--- Baekjoon/2577.py
+++ Baekjoon/2577.py
@@ -1,4 +1,8 @@
 # 숫자의 개수
-from math import prod
-a = str(prod((int(input()) for _ in range(3))))
-print(*{k: a.count(str(k)) for k in range(10)}.values())
+import math
+n = math.prod(int(input()) for _ in range(3))
+a = [0] * 10
+while n:
+    n, r = divmod(n, 10)
+    a[r] += 1
+print(*a)
