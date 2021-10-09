9dbcbc7d135f356e86cc126934887e74610e6cd1
J-JunShim <jjunsh.120@gmail.com>
2021-10-09 18:53:55 +0900

    Update 1065.py

--- Baekjoon/1065.py
+++ Baekjoon/1065.py
@@ -1,7 +1,8 @@
 # 한수
-def a(n: int) -> bool:
-    return n // 10 % 10 * 2 == n // 100 + n % 10
+def f(x):
+    q, r = divmod(x, 10)
+    return q // 10 + r == q % 10 * 2
 
 
 n = int(input())
-print(n if n < 100 else 99 + sum(a(i) for i in range(100, n + 1)))
+print(n if n < 100 else sum(map(f, range(100, n + 1))) + 99)
